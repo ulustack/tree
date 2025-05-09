74283345302d44b9f37855aef66818ca303cb10c
yusuf <yusuf@DESKTOP-L63CBC9>
2025-05-09 18:12:44 +0300

    Add project files.

---
+++ tree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TreeNode<T>
+{
+    public T Data { get; set; }
+    public TreeNode<T> Parent { get; set; }
+    public List<TreeNode<T>> Children { get; set; }
+
+    public TreeNode(T data, TreeNode<T> parent = null)
+    {
+        Data = data;
+        Parent = parent;
+        Children = new List<TreeNode<T>>();
+    }
+
+    public void AddChild(TreeNode<T> child)
+    {
+        child.Parent = this;
+        Children.Add(child);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"- {Data}");
+        foreach (var child in Children)
+        {
+            Console.WriteLine($" - {child.Data}");
+        }
+    }
+}
